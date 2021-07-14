3c3efe85aa2300ee90238d464a273a1fc502e68d
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-14 13:03:15 -0400

    Added routes for reservations

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ const PORT = 3000;
 app.use(express.json());
 
 // --------- Routes --------- //
+app.get('/', (req, res) => res.send('Does this even work tho?'));
+
+app.get('/api/reservations', (req, res) => res.json(characters));
+
+app.get ('api/reservations/:reservation', (req, res) => {
+    const chosen = req.params.reservation;
+
+    console.log(reservation);
+
+    for (let i = 0; i < reservations.length; i++) {
+        if (chosen === reservations[i].routeName) {
+            return res.json(reservations[i]);
+        }
+    }
+});
