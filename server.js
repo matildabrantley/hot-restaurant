a7004eb04bb31a8227709df4555a757edad8479d
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-14 13:41:30 -0400

    Created new route for reserve.html

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 
-app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
 
 app.get('/api/reservations', (req, res) => res.json(reservations));
 
