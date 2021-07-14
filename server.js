ca0cffb30907fcd5d36e81d1b9aa80031095941c
Matilda Brantley <matildahbrantley@gmail.com>
2021-07-14 13:00:05 -0400

    added post method to add new reservation from user input

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require('express');
 
+
+
+
+
+
+
+// Create table reservation
+app.post('/api/reservations', (req, res) => {
+    const newReservation = req.body;
+  
+    reservations.push(newReservation);
+
+    res.json(newReservation);
+  });
