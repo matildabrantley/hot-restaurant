15a9a4ea74968985aa90d210064cc8339a5e81c4
Austin Baxley <ausbaxley@gmail.com>
2021-07-14 13:01:48 -0400

    pull

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
+// dependencies
 const express = require('express');
 
+// setting up express app
+
+const app = express();
+const PORT = 8080;
+
+// server starter
+app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
